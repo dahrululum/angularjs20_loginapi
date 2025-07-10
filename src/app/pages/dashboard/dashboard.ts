517a8a9b25c97c284b79e0871946fd5fdfaa6c10
dahrululum <dahrul.ulum@gmail.com>
2025-07-10 09:57:22 +0700

    pagination on dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,31 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { AngularPaginatorModule } from 'angular-paginator';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [AngularPaginatorModule,CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
     http = inject(HttpClient);
-    constructor(){
+    userList: any[] = [];
+    todoList: any[] = [];
+    postList: any[] = [];
+
+    currentPage = 1;
+    maxPage = 10;
+
+
+    ngOnInit(): void {
       this.getUsers();
+      this.getTodoItems();
+      this.getPosts();
+    }
+
+    constructor(){
+      
     }
     getUsers(){
-      //debugger;
-      this.http.get("https://freeapi.miniprojectideas.com/api/User/GetAllUsers").subscribe(
-      {
-      next:(response:any)=>{
-        //debugger;
-          
-        },
-        error:(error)=>{
-          //debugger;
-        
-        }
-      }
-      )
+     
+      this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((result:any) => {
+      
+        this.userList = result;
+      })
     }
+    getTodoItems(){
+        this.http.get("https://jsonplaceholder.typicode.com/todos").subscribe((response:any) =>{
+        this.todoList =response;
+      })
+    }
+
+    getPosts(){
+        this.http.get("https://jsonplaceholder.typicode.com/posts").subscribe((response:any) =>{
+        this.postList =response;
+      })
+    }
+    
 }
